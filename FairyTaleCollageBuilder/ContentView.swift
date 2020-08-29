293d0c628f6e40ff50e6ed99d6bafe6bc0a184ab
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2020-08-29 09:32:23 +0300

    Initial Commit

---
+++ FairyTaleCollageBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FairyTaleCollageBuilder
+//
+//  Created by Stanimira Vlaeva on 29.08.20.
+//  Copyright © 2020 Stanimira Vlaeva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
